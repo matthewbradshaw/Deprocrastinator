6f149d828c961e51dcccef0d90906b7810c15d17
matthewbradshaw <matthew.m.bradshaw@gmail.com>
2015-01-12 13:53:43 -0600

    Initial Commit

---
+++ Deprocrastinator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Deprocrastinator
+//
+//  Created by Matthew Bradshaw on 1/12/15.
+//  Copyright (c) 2015 Matthew Bradshaw. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Deprocrastinator/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Deprocrastinator
+//
+//  Created by Matthew Bradshaw on 1/12/15.
+//  Copyright (c) 2015 Matthew Bradshaw. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
