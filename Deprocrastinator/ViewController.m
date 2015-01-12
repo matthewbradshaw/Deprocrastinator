8a0e50ab22e204a7eb0bcc9842fe57476a4d8eee
matthewbradshaw <matthew.m.bradshaw@gmail.com>
2015-01-12 15:19:44 -0600

    Completed steps 1 & 2

--- Deprocrastinator/ViewController.m
+++ Deprocrastinator/ViewController.m
@@ -8,16 +8,60 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController () <UITableViewDelegate, UITableViewDataSource>
+
+@property (strong, nonatomic) IBOutlet UITextField *userEntry;
+@property NSMutableArray *userEntries;
+@property (strong, nonatomic) IBOutlet UITableView *userEntryTableView;
 
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
+    NSLog(@"<%@:%@:%d>", NSStringFromClass([self class]), NSStringFromSelector(_cmd), __LINE__);
+
     [super viewDidLoad];
 
+    self.userEntries = [NSMutableArray arrayWithObjects: nil];
+
 }
+
+- (IBAction)onAddButtonPressed:(UIButton *)sender {
+    NSLog(@"<%@:%@:%d>", NSStringFromClass([self class]), NSStringFromSelector(_cmd), __LINE__);
+
+    if (self.userEntry.text) {
+        NSString *myUserEntry = self.userEntry.text;
+        [self.userEntries addObject:myUserEntry];
+        NSLog(@"my user entry %@", myUserEntry);
+        NSLog(@"self.userEntries.count %lu", (unsigned long)self.userEntries.count);
+        [self.userEntryTableView reloadData];
+//        self.userEntry.text = @"";
+    }
+
+
+
+
+//    if (self.userEntry.text) {
+//        NSLog(@"We Have Entry %@", myUserEntry);
+//    }
+}
+
+-(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    NSLog(@"<%@:%@:%d>", NSStringFromClass([self class]), NSStringFromSelector(_cmd), __LINE__);
+
+    return self.userEntries.count;
+}
+
+-(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    NSLog(@"<%@:%@:%d>", NSStringFromClass([self class]), NSStringFromSelector(_cmd), __LINE__);
+
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"ToDoCellID"];
+    cell.textLabel.text = [NSString stringWithFormat:@"%@", self.userEntry.text];
+
+    return cell;
+}
+
 
 
 
