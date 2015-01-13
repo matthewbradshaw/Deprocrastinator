5e712563f2a5f9eab3484c135b871d57758c39ea
matthewbradshaw <matthew.m.bradshaw@gmail.com>
2015-01-12 19:04:48 -0600

    Edit delete rows logic

--- Deprocrastinator/ViewController.m
+++ Deprocrastinator/ViewController.m
@@ -44,8 +44,46 @@
     [self.userEntryTableView reloadData];
 
 }
+#pragma mark OnEdit
+-(BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath {
+    return YES;
+}
+-(BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath {
+    return YES;
+}
+-(void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)sourceIndexPath toIndexPath:(NSIndexPath *)destinationIndexPath{
+    NSString *titleItem = [self.userEntries objectAtIndex:sourceIndexPath.row];
+    [self.userEntries removeObject:titleItem];
+    [self.userEntries insertObject:titleItem atIndex:destinationIndexPath.row];
+
+}
+
+-(void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath{
+    [self.userEntries removeObjectAtIndex:indexPath.row];
+    [self.userEntryTableView reloadData];
+}
+
+- (IBAction)onEditButtonPressed:(UIBarButtonItem *)sender {
+
+    if (self.userEntryTableView.editing) {
+        self.editing = false;
+        [self.userEntryTableView setEditing:NO animated:YES];
+        sender.style = UIBarButtonItemStylePlain;
+        sender.title = @"Edit";
+    } else {
+        self.editing = true;
+        [self.userEntryTableView setEditing:YES animated:YES];
+        sender.style = UIBarButtonItemStyleDone;
+        sender.title = @"Done";
+        [sender setTitle:@"Done"];
+    }
+
+}
+
 
 #pragma mark UITableViewDataSource
+
+
 
 -(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
     NSLog(@"<%@:%@:%d>", NSStringFromClass([self class]), NSStringFromSelector(_cmd), __LINE__);
